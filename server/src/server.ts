6e2c84c9bd0d0a6d15c60953365b7a814b022c31
LeonardoCardosoDev <leonardo.cardoso@sptech.school>
2023-01-18 02:03:57 -0300

    create register cors

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const app = Fastify();
+app.register(cors);
+
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany();
+  return habits;
+});
+
+app.listen({
+  port: 3333,
+});
